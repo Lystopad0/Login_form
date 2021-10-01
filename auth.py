9754ca9051a619e270b034db5da9d7b13cf69842
Sasha <fantom.sasha@gmail.com>
2021-10-01 13:29:47 +0300

    create SignUp

---
+++ auth.py
@@ -0,0 +1,73 @@
+from fastapi import APIRouter, status, Depends
+from databaseSQ import Session, engine
+from schemas import SignUp, LoginModel
+from model import User
+from fastapi.exceptions import HTTPException
+from werkzeug.security import generate_password_hash, check_password_hash
+from fastapi_jwt_auth import AuthJWT
+from fastapi.encoders import jsonable_encoder
+
+
+auth = APIRouter(
+    prefix='/auth',
+    tags=['auth']
+)
+
+session = Session(bind=engine)
+
+@auth.get('/')
+async def try_login(Authorize: AuthJWT = Depends()):
+    '''
+        ## Try this if you login
+    '''
+    try:
+        Authorize.jwt_required()
+
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Invalid token')
+
+    return {'massage': 'Hello User'}
+
+
+@auth.post('/signup', status_code=status.HTTP_201_CREATED)
+async def signup(user: SignUp):
+    """
+            ## Create a user
+            This requires the following
+            ```
+                    name: str
+                    username:str
+                    email:str
+                    password:str
+                    is_staff:bool
+                    is_active:bool
+            ```
+        """
+
+    db_email = session.query(User).filter(User.email == user.email).first()
+    if db_email is not None:
+        return HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                             detail='User with the email already exists')
+
+    db_username = session.query(User).filter(User.username == user.username).first()
+
+    if db_username is not None:
+        return HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                             detail='User with the username already exists')
+
+    new_user = User(
+        name = user.name,
+        username = user.username,
+        email = user.email,
+        password = generate_password_hash(user.password),
+        is_active = user.is_active
+    )
+
+    session.add(new_user)
+
+    session.commit()
+
+    return new_user
+
+
--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from auth import auth
+from fastapi_jwt_auth import AuthJWT
+from schemas import Settings
+# import inspect
+# import re
+# from fastapi.routing import APIRoute
+# from fastapi.openapi.utils import get_openapi
 
 app = FastAPI()
 
 
-@app.get('/')
-def hello():
-    return {'massage': 'hello'}
+
+
+@AuthJWT.load_config
+def get_config():
+    return Settings()
+
+
+app.include_router(auth)
